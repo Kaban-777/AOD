3712dd3b9eff018d15f00718808f50ffc7059d26
Kaban-777 <81770249+Kaban-777@users.noreply.github.com>
2022-03-18 16:55:55 +0300

    Add files via upload

---
+++ Fourth sem/Python/PR3/1.py
@@ -0,0 +1,16 @@
+def finction(x =1):  # 4
+    return x
+
+def main ():  # 1, 5
+    print(1); print(2)  # 7
+    x = 1,2  # 3
+    if x > 5: y = 10  # 6
+    b = False
+    if b == None:  # 8
+        return b
+    if b == True:  # 9
+        return b
+
+
+if __name__== '__main__':  # 2
+    main()
